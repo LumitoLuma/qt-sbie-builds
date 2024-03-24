e979d20280c9881b8c96f35e2c5b97ac31ab9317
Lumito <lumito@lumito.net>
2024-03-24 23:49:33 +0000

    Fix

--- build_sources.cmd
+++ build_sources.cmd
@@ -1,7 +1,7 @@
 if %1 == x64 (
     set build_arch=x64
+    set PATH=%PATH%;%~dp0qt-everywhere-src-5.15.13\qtbase\bin;%~dp0qt-everywhere-src-5.15.13\gnuwin32\bin;%~dp0jom
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-    set PATH=%~dp0qt-everywhere-src-5.15.13\qtbase\bin;%~dp0qt-everywhere-src-5.15.13\gnuwin32\bin;%~dp0jom;"%PATH%"
     pushd %~dp0qt-everywhere-src-5.15.13\
     call configure.bat -release -opensource -confirm-license -prefix %~dp0bin\5.15.13\msvc2019_64 -platform win32-msvc -nomake tests -nomake examples -skip qtdoc -skip qtwebengine
     jom
@@ -10,8 +10,8 @@ if %1 == x64 (
 
 if %1 == Win32 (
     set build_arch=x86
+    set PATH=%PATH%;%~dp0qt-everywhere-src-5.15.13\qtbase\bin;%~dp0qt-everywhere-src-5.15.13\gnuwin32\bin;%~dp0jom
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
-    set PATH=%~dp0qt-everywhere-src-5.15.13\qtbase\bin;%~dp0qt-everywhere-src-5.15.13\gnuwin32\bin;%~dp0jom;"%PATH%"
     pushd %~dp0qt-everywhere-src-5.15.13\
     call configure.bat -release -opensource -confirm-license -prefix %~dp0bin\5.15.13\msvc2019 -platform win32-msvc -nomake tests -nomake examples -skip qtdoc -skip qtwebengine
     jom
