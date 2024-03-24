f4e80a7008858f36fbdfdbc48843580a425e3792
Lumito <lumito@lumito.net>
2024-03-24 23:22:32 +0000

    Update

--- build_sources.cmd
+++ build_sources.cmd
@@ -7,3 +7,13 @@ if %1 == x64 (
     jom
     jom install
 )
+
+if %1 == Win32 (
+    set build_arch=x86
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
+    set PATH=%~dp0qt-everywhere-src-5.15.13\qtbase\bin;%~dp0qt-everywhere-src-5.15.13\gnuwin32\bin;%~dp0jom;%PATH%
+    pushd %~dp0qt-everywhere-src-5.15.13\
+    call configure.bat -release -opensource -confirm-license -prefix %~dp0bin\5.15.13\msvc2019 -nomake tests -nomake examples -skip qtdoc -skip qtwebengine
+    jom
+    jom install
+)
