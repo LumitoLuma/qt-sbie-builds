621c061fde38891f0d9aee1479788aa8670ec299
Lumito <lumito@lumito.net>
2024-03-25 11:11:07 +0000

    Attempt fix long path

--- build_sources.cmd
+++ build_sources.cmd
@@ -1,8 +1,8 @@
 if %1 == x64 (
     set build_arch=x64
-    set "PATH=%~dp0qt-everywhere-src-5.15.13\qtbase\bin;%~dp0qt-everywhere-src-5.15.13\gnuwin32\bin;%~dp0jom;%PATH%"
+    set "PATH=%~dp0src\qtbase\bin;%~dp0src\gnuwin32\bin;%~dp0jom;%PATH%"
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-    pushd %~dp0qt-everywhere-src-5.15.13\
+    pushd %~dp0src\
     call configure.bat -release -opensource -confirm-license -prefix %~dp0bin\5.15.13\msvc2019_64 -platform win32-msvc -nomake tests -nomake examples -skip qtdoc -skip qtwebengine
     jom
     if %ERRORLEVEL% == 0 jom install
@@ -10,9 +10,9 @@ if %1 == x64 (
 
 if %1 == Win32 (
     set build_arch=x86
-    set "PATH=%~dp0qt-everywhere-src-5.15.13\qtbase\bin;%~dp0qt-everywhere-src-5.15.13\gnuwin32\bin;%~dp0jom;%PATH%"
+    set "PATH=%~dp0src\qtbase\bin;%~dp0src\gnuwin32\bin;%~dp0jom;%PATH%"
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
-    pushd %~dp0qt-everywhere-src-5.15.13\
+    pushd %~dp0src\
     call configure.bat -release -opensource -confirm-license -prefix %~dp0bin\5.15.13\msvc2019 -platform win32-msvc -nomake tests -nomake examples -skip qtdoc -skip qtwebengine
     jom
     if %ERRORLEVEL% == 0 jom install
--- download_sources.cmd
+++ download_sources.cmd
@@ -3,41 +3,42 @@ if exist %~dp0qt-everywhere-src-5.15.13\configure.bat goto done
 REM Downloading Qt 5.15.13 source code...
 curl -LsSO --output-dir %~dp0 https://download.qt.io/archive/qt/5.15/5.15.13/single/qt-everywhere-opensource-src-5.15.13.zip
 "C:\Program Files\7-Zip\7z.exe" x -aoa -o%~dp0 %~dp0qt-everywhere-opensource-src-5.15.13.zip
+ren %~dp0qt-everywhere-src-5.15.13 %~dp0src
 
 REM Downloading Qt 5.15.13 security patches...
-REM curl -LsSO --output-dir %~dp0qt-everywhere-src-5.15.13\qtsvg\ https://download.qt.io/archive/qt/5.15/CVE-2023-32573-qtsvg-5.15.diff
-REM curl -LsSO --output-dir %~dp0qt-everywhere-src-5.15.13\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-32762-qtbase-5.15.diff
-REM curl -LsSO --output-dir %~dp0qt-everywhere-src-5.15.13\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-32763-qtbase-5.15.diff
-REM curl -LsSO --output-dir %~dp0qt-everywhere-src-5.15.13\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-33285-qtbase-5.15.diff
-REM curl -LsSO --output-dir %~dp0qt-everywhere-src-5.15.13\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-34410-qtbase-5.15.diff
-REM curl -LsSO --output-dir %~dp0qt-everywhere-src-5.15.13\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-37369-qtbase-5.15.diff
-REM curl -LsSO --output-dir %~dp0qt-everywhere-src-5.15.13\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-38197-qtbase-5.15.diff
-REM curl -LsSO --output-dir %~dp0qt-everywhere-src-5.15.13\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-43114-5.15.patch
-REM curl -LsSO --output-dir %~dp0qt-everywhere-src-5.15.13\qtbase\ https://download.qt.io/archive/qt/5.15/0001-CVE-2023-51714-qtbase-5.15.diff
-REM curl -LsSO --output-dir %~dp0qt-everywhere-src-5.15.13\qtbase\ https://download.qt.io/archive/qt/5.15/0002-CVE-2023-51714-qtbase-5.15.diff
-REM curl -LsSO --output-dir %~dp0qt-everywhere-src-5.15.13\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2024-25580-qtbase-5.15.diff
+curl -LsSO --output-dir %~dp0src\qtsvg\ https://download.qt.io/archive/qt/5.15/CVE-2023-32573-qtsvg-5.15.diff
+curl -LsSO --output-dir %~dp0src\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-32762-qtbase-5.15.diff
+curl -LsSO --output-dir %~dp0src\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-32763-qtbase-5.15.diff
+curl -LsSO --output-dir %~dp0src\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-33285-qtbase-5.15.diff
+curl -LsSO --output-dir %~dp0src\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-34410-qtbase-5.15.diff
+curl -LsSO --output-dir %~dp0src\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-37369-qtbase-5.15.diff
+curl -LsSO --output-dir %~dp0src\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-38197-qtbase-5.15.diff
+curl -LsSO --output-dir %~dp0src\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2023-43114-5.15.patch
+curl -LsSO --output-dir %~dp0src\qtbase\ https://download.qt.io/archive/qt/5.15/0001-CVE-2023-51714-qtbase-5.15.diff
+curl -LsSO --output-dir %~dp0src\qtbase\ https://download.qt.io/archive/qt/5.15/0002-CVE-2023-51714-qtbase-5.15.diff
+curl -LsSO --output-dir %~dp0src\qtbase\ https://download.qt.io/archive/qt/5.15/CVE-2024-25580-qtbase-5.15.diff
 
 REM Install GNU Patch tool (from Chocolatey)...
-REM choco install patch -y
+choco install patch -y
 
 REM Patch Qt sources...
-REM pushd %~dp0qt-everywhere-src-5.15.13\qtbase\
-REM patch -p1 -i CVE-2023-32762-qtbase-5.15.diff
-REM patch -p1 -i CVE-2023-32763-qtbase-5.15.diff
-REM patch -p1 -i CVE-2023-33285-qtbase-5.15.diff
-REM patch -p1 -i CVE-2023-34410-qtbase-5.15.diff
-REM patch -p1 -i CVE-2023-37369-qtbase-5.15.diff
-REM patch -p1 -i CVE-2023-38197-qtbase-5.15.diff
-REM patch -p1 -i CVE-2023-43114-5.15.patch
-REM patch -p1 -i 0001-CVE-2023-51714-qtbase-5.15.diff
-REM patch -p1 -i 0002-CVE-2023-51714-qtbase-5.15.diff
-REM patch -p1 -i CVE-2024-25580-qtbase-5.15.diff
-REM popd
-REM pushd %~dp0qt-everywhere-src-5.15.13\qtsvg\
-REM patch -p1 -i CVE-2023-32573-qtsvg-5.15.diff
-REM popd
+pushd %~dp0src\qtbase\
+patch -p1 -i CVE-2023-32762-qtbase-5.15.diff
+patch -p1 -i CVE-2023-32763-qtbase-5.15.diff
+patch -p1 -i CVE-2023-33285-qtbase-5.15.diff
+patch -p1 -i CVE-2023-34410-qtbase-5.15.diff
+patch -p1 -i CVE-2023-37369-qtbase-5.15.diff
+patch -p1 -i CVE-2023-38197-qtbase-5.15.diff
+patch -p1 -i CVE-2023-43114-5.15.patch
+patch -p1 -i 0001-CVE-2023-51714-qtbase-5.15.diff
+patch -p1 -i 0002-CVE-2023-51714-qtbase-5.15.diff
+patch -p1 -i CVE-2024-25580-qtbase-5.15.diff
+popd
+pushd %~dp0src\qtsvg\
+patch -p1 -i CVE-2023-32573-qtsvg-5.15.diff
+popd
 
 :done
 
 REM dir %~dp0
-REM dir %~dp0qt-everywhere-src-5.15.13\
+REM dir %~dp0src\
