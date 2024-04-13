58810ff0340be868bc1a7246593f2ba9513cc29a
Lumito <lumito@lumito.net>
2024-04-13 19:59:51 +0200

    Use nmake

--- build_sources.cmd
+++ build_sources.cmd
@@ -4,8 +4,8 @@ if %1 == x64 (
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
     pushd %~dp0src\openssl\
     perl Configure shared no-asm --prefix=%~dp0openssl-win64 VC-WIN64A
-    jom
-    jom install
+    nmake
+    nmake install
     popd
     pushd %~dp0src\
     call configure.bat -release -opensource -confirm-license -prefix %~dp0bin\5.15.13\msvc2019_64 -platform win32-msvc -openssl-linked OPENSSL_PREFIX="%~dp0openssl-win64" -nomake tests -nomake examples -skip qtdoc -skip qtwebengine
@@ -20,8 +20,8 @@ if %1 == Win32 (
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
 pushd %~dp0src\openssl\
     perl Configure shared no-asm --prefix=%~dp0openssl-win32 VC-WIN32
-    jom
-    jom install
+    nmake
+    nmake install
     popd
     pushd %~dp0src\
     call configure.bat -release -opensource -confirm-license -prefix %~dp0bin\5.15.13\msvc2019 -platform win32-msvc -openssl-linked OPENSSL_PREFIX="%~dp0openssl-win32" -nomake tests -nomake examples -skip qtdoc -skip qtwebengine
