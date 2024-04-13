ed2826bf82f8ad45a48505fe2bd52c8c478ff802
Lumito <lumito@lumito.net>
2024-04-13 19:11:37 +0200

    Fix

--- build_sources.cmd
+++ build_sources.cmd
@@ -3,7 +3,7 @@ if %1 == x64 (
     set "PATH=%~dp0src\qtbase\bin;%~dp0src\gnuwin32\bin;%~dp0jom;%PATH%"
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
     pushd %~dp0src\
-    call configure.bat -release -opensource -confirm-license -prefix %~dp0bin\5.15.13\msvc2019_64 -platform win32-msvc -openssl-runtime OPENSSL_INCDIR=%~dp0src\openssl\include -nomake tests -nomake examples -skip qtdoc -skip qtwebengine
+    call configure.bat -release -opensource -confirm-license -prefix %~dp0bin\5.15.13\msvc2019_64 -platform win32-msvc -openssl-runtime OPENSSL_INCDIR="%~dp0src\openssl\include" -I "%~dp0src\openssl\include" -nomake tests -nomake examples -skip qtdoc -skip qtwebengine
     jom
     if %ERRORLEVEL% == 0 jom install
     if %ERRORLEVEL% == 0 mkdir %~dp0archive && "C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 -mfb=273 -ms -md=31 -myx=9 -mtm=- -mmt -mmtf -md=1536m -mmf=bt3 -mmc=10000 -mpb=0 -mlc=0 %~dp0archive\qt-everywhere-5.15.13-Windows_10-MSVC2019-x86_64.7z %~dp0bin\5.15.13
@@ -14,7 +14,7 @@ if %1 == Win32 (
     set "PATH=%~dp0src\qtbase\bin;%~dp0src\gnuwin32\bin;%~dp0jom;%PATH%"
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
     pushd %~dp0src\
-    call configure.bat -release -opensource -confirm-license -prefix %~dp0bin\5.15.13\msvc2019 -platform win32-msvc -openssl-runtime OPENSSL_INCDIR=%~dp0src\openssl\include -nomake tests -nomake examples -skip qtdoc -skip qtwebengine
+    call configure.bat -release -opensource -confirm-license -prefix %~dp0bin\5.15.13\msvc2019 -platform win32-msvc -openssl-runtime OPENSSL_INCDIR="%~dp0src\openssl\include" -I "%~dp0src\openssl\include" -nomake tests -nomake examples -skip qtdoc -skip qtwebengine
     jom
     if %ERRORLEVEL% == 0 jom install
     if %ERRORLEVEL% == 0 mkdir %~dp0archive && "C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 -mfb=273 -ms -md=31 -myx=9 -mtm=- -mmt -mmtf -md=1536m -mmf=bt3 -mmc=10000 -mpb=0 -mlc=0 %~dp0archive\qt-everywhere-5.15.13-Windows_10-MSVC2019-x86.7z %~dp0bin\5.15.13
