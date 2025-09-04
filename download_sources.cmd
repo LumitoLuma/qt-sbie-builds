56df4c700c79273434f4d3ca395833281f4860b6
Lumito <lumito@lumito.net>
2025-09-04 14:46:12 +0000

    Switch to Qt 6.8.3 with crystalidea's Win7 patches

--- download_sources.cmd
+++ download_sources.cmd
@@ -1,7 +1,7 @@
 echo %*
 set "qt_version=%2"
-set "qt_source_url=https://download.qt.io/archive/qt/5.15/%qt_version%/single/qt-everywhere-opensource-src-%qt_version%.zip"
-set "qt_patch_url=https://download.qt.io/archive/qt/5.15"
+set "qt_source_url=https://download.qt.io/archive/qt/6.8/%qt_version%/single/qt-everywhere-src-%qt_version%.zip"
+set "qt_patch_url=https://download.qt.io/archive/qt/6.8"
 set "openssl_version=%3"
 set "openssl_source_url=https://github.com/openssl/openssl/releases/download/openssl-%openssl_version%/openssl-%openssl_version%.tar.gz"
 set "bDir=%~dp0"
@@ -17,47 +17,30 @@ if "%EOL%" == "LF" (
 
 if exist "%bDir%\qt-everywhere-src-%qt_version%\configure.bat" goto done
 
-REM Downloading Qt 5.15 source code...
+REM Downloading Qt 6 source code...
 echo Downloading Qt %qt_version% source code...
 curl -LsSO --output-dir "%bDir%" "%qt_source_url%"
 "C:\Program Files\7-Zip\7z.exe" x -aoa -o"%bDir%" "%bDir%\qt-everywhere-opensource-src-%qt_version%.zip"
 move "%bDir%\qt-everywhere-src-%qt_version%" "%bDir%\src"
 
-REM Downloading Qt 5.15 security patches...
+REM Downloading Qt 6 patches for Win7
+curl -LsSO --output-dir "%bDir%" "https://github.com/crystalidea/qt6windows7/archive/refs/tags/v6.8.3.zip"
+"C:\Program Files\7-Zip\7z.exe" x -aoa -o"%bDir%" "%bDir%\v6.8.3.zip"
+xcopy "%bDir%\qt6windows7-6.8.3\qtbase" "%bDir%\src\qtbase" /S /H /Q /Y /R /E
+
+REM Downloading Qt 6 security patches...
 echo Downloading Qt %qt_version% security patches...
 
-if "%qt_version%" == "5.15.13" echo Qt version is %qt_version% & goto verMin13
-if "%qt_version%" == "5.15.14" echo Qt version is %qt_version% & goto verMin14
-if "%qt_version%" == "5.15.15" echo Qt version is %qt_version% & goto verMin15
-if "%qt_version%" == "5.15.16" echo Qt version is %qt_version% & goto verMin16
-if "%qt_version%" == "5.15.17" echo Qt version is %qt_version% & goto verMin17
-if "%qt_version%" == "5.15.18" echo Qt version is %qt_version% & goto verMin18
-if "%qt_version%" == "5.15.19" echo Qt version is %qt_version% & goto verMin19
+if "%qt_version%" == "6.8.3" echo Qt version is %qt_version% & goto verMin3
 
-:verMin13
-curl -LsSO --output-dir "%bDir%\src\qtsvg" "%qt_patch_url%/CVE-2023-32573-qtsvg-5.15.diff"
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2023-32762-qtbase-5.15.diff"
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2023-33285-qtbase-5.15.diff"
-:verMin14
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2023-32763-qtbase-5.15.diff"
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2023-34410-qtbase-5.15.diff"
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2023-37369-qtbase-5.15.diff"
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2023-38197-qtbase-5.15.diff"
-:verMin15
-curl -LsSO --output-dir "%bDir%\src\qtimageformats" "%qt_patch_url%/CVE-2023-4863-5.15.patch"
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2023-43114-5.15.patch"
-:verMin16
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/0001-CVE-2023-51714-qtbase-5.15.diff"
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/0002-CVE-2023-51714-qtbase-5.15.diff"
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2024-25580-qtbase-5.15.diff"
-curl -LsSO --output-dir "%bDir%\src\qtnetworkauth" "%qt_patch_url%/CVE-2024-36048-qtnetworkauth-5.15.diff"
-:verMin17
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2024-39936-qtbase-5.15.patch"
-:verMin18
-curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2025-30348-qtbase-5.15.diff"
-curl -LsSO --output-dir "%bDir%\src\qtconnectivity" "%qt_patch_url%/CVE-2025-23050-qtconnectivity-5.15.diff"
-:verMin19
-
+:verMin3
+curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2025-3512-qtbase-6.8.diff"
+curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2025-4211-qtbase-6.8.diff"
+curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2025-5455-qtbase-6.8.patch"
+curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2025-5992-qtbase-6.8.patch"
+curl -LsSO --output-dir "%bDir%\src\qtbase" "%qt_patch_url%/CVE-2025-6338-qtbase-6.8.patch"
+curl -LsSO --output-dir "%bDir%\src\qtconnectivity" "%qt_patch_url%/CVE-2025-23050-qtconnectivity-6.8.diff"
+curl -LsSO --output-dir "%bDir%\src\qtimageformats" "%qt_patch_url%/CVE-2025-5683-qtimageformats-6.8.patch"
 
 REM Install GNU Patch tool (from Chocolatey)...
 choco install patch -y
@@ -66,21 +49,9 @@ REM Install Dos2Unix / Unix2Dos - Text file format converters
 choco install dos2unix -y
 
 REM Patch Qt sources...
-pushd "%bDir%\src\qtsvg"
-set qtsvg_patches="CVE-2023-32573-qtsvg-5.15.diff"
-for %%i in (%qtsvg_patches%) do (
-    if exist "%%i" (
-        echo Applying patch "%%i"
-        %EOLconverter% --verbose "%%i"
-        patch -p1 --verbose -u -i "%%i"
-    ) else (
-        echo File "%%i" does not exist. Skipping...
-    )
-)
-popd
 
 pushd "%bDir%\src\qtbase"
-set qtbase_patches="CVE-2023-32762-qtbase-5.15.diff" "CVE-2023-33285-qtbase-5.15.diff" "CVE-2023-32763-qtbase-5.15.diff" "CVE-2023-34410-qtbase-5.15.diff" "CVE-2023-37369-qtbase-5.15.diff" "CVE-2023-38197-qtbase-5.15.diff" "CVE-2023-43114-5.15.patch" "0001-CVE-2023-51714-qtbase-5.15.diff" "0002-CVE-2023-51714-qtbase-5.15.diff" "CVE-2024-25580-qtbase-5.15.diff" "CVE-2024-39936-qtbase-5.15.patch" "CVE-2025-30348-qtbase-5.15.diff"
+set qtbase_patches="CVE-2025-3512-qtbase-6.8.diff" "CVE-2025-4211-qtbase-6.8.diff" "CVE-2025-5455-qtbase-6.8.patch" "CVE-2025-5992-qtbase-6.8.patch" "CVE-2025-6338-qtbase-6.8.patch"
 for %%i in (%qtbase_patches%) do (
     if exist "%%i" (
         echo Applying patch "%%i"
@@ -93,7 +64,7 @@ for %%i in (%qtbase_patches%) do (
 popd
 
 pushd "%bDir%\src\qtimageformats"
-set qtimageformats_patches="CVE-2023-4863-5.15.patch"
+set qtimageformats_patches="CVE-2025-5683-qtimageformats-6.8.patch"
 for %%i in (%qtimageformats_patches%) do (
     if exist "%%i" (
         echo Applying patch "%%i"
@@ -105,21 +76,8 @@ for %%i in (%qtimageformats_patches%) do (
 )
 popd
 
-pushd "%bDir%\src\qtnetworkauth"
-set qtnetworkauth_patches="CVE-2024-36048-qtnetworkauth-5.15.diff"
-for %%i in (%qtnetworkauth_patches%) do (
-    if exist "%%i" (
-        echo Applying patch "%%i"
-        %EOLconverter% --verbose "%%i"
-        patch -p1 --verbose -u -i "%%i"
-    ) else (
-        echo File "%%i" does not exist. Skipping...
-    )
-)
-popd
-
 pushd "%bDir%\src\qtconnectivity"
-set qtconnectivity_patches="CVE-2025-23050-qtconnectivity-5.15.diff"
+set qtconnectivity_patches="CVE-2025-23050-qtconnectivity-6.8.diff"
 for %%i in (%qtconnectivity_patches%) do (
     if exist "%%i" (
         echo Applying patch "%%i"
